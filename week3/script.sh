63b0ec3867f323546bf736800cb255a563245e38
jzburns <jzburns@gmail.com>
2014-10-10 11:32:17 +0100

    week4 work

---
+++ week3/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Title : script.sh
+# Date : 
+# Author : Raymond Creevey
+# Version :
+# Description : 
+# Options :
+
+#use a printf statment here to print 
+#a user-friendly message
+
+
+#this line produces a 2
+grep processor /proc/cpuinfo | wc -l
+
