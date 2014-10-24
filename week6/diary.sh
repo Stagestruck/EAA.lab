a891aba4b5307a4a9a5c3a85bffcbb0e770f9a38
Stagestruck <ray.stagestruck@gmail.com>
2014-10-24 11:15:16 +0100

    week6 x

--- week6/diary.sh
+++ week6/diary.sh
@@ -7,6 +7,14 @@
 
 # append to diary.txt file using >>
 
-#   >> diary.txt
+#cnt=$( grep processor/proc/cpuinfo | wc -l )
+#printf "%s cpu" $cnt
+#if [ $cnt<4 ]
+#then printf "Too few cores (%s) |n" $cen  >> diary.txt
+read txt
+printf "%s is the input\n" $txt
+thedate=$( date )
+print "%s %s" "$thedate" $txt
+
 
 
